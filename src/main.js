e982f2b377eb30ae9b55aa7a0915ffae5ee71095
annplay <messageforannolay@gmail.com>
2025-02-23 18:53:07 +0200

    003

--- src/main.js
+++ src/main.js
@@ -1,9 +1,7 @@
 import iziToast from 'izitoast';
 import 'izitoast/dist/css/iziToast.min.css';
-
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
-
 import { fetchImages } from './js/pixabay-api';
 import { imagesTemplate } from './js/render-functions';
 
@@ -11,16 +9,21 @@ const refs = {
   container: document.querySelector('.gallery'),
   form: document.querySelector('.form'),
   loader: document.querySelector('.loader'),
+  loadMoreBtn: document.querySelector('.load-more'),
 };
 
-refs.form.addEventListener('submit', e => {
-  e.preventDefault();
+let query = '';
+let page = 1;
+const perPage = 40;
 
-  const inputValue = e.target.elements.text.value.trim();
-  if (!inputValue) {
+refs.form.addEventListener('submit', async (e) => {
+  e.preventDefault();
+  
+  query = e.target.elements.text.value.trim();
+  if (!query) {
     iziToast.error({
       title: 'Error',
-      message: 'Something went wrong. Please try again later!',
+      message: 'Please enter a search query!',
       position: 'topRight',
     });
     return;
@@ -28,29 +31,47 @@ refs.form.addEventListener('submit', e => {
 
   refs.container.innerHTML = '';
   refs.loader.classList.remove('hidden');
-
-  fetchImages(inputValue)
-    .then(images => {
-      renderValue(images);
-    })
-    .catch(error => {
-      iziToast.error({
-        title: 'Error',
-        message:
-          'Sorry, there are no images matching your search query. Please try again!',
-        position: 'topRight',
-      });
-    })
-    .finally(() => {
-      refs.loader.classList.add('hidden');
-      e.target.reset();
+  refs.loadMoreBtn.classList.add('hidden');
+  page = 1;
+  
+  try {
+    const images = await fetchImages(query, page, perPage);
+    renderImages(images);
+    if (images.length === perPage) refs.loadMoreBtn.classList.remove('hidden');
+  } catch (error) {
+    iziToast.error({
+      title: 'Error',
+      message: 'No images found. Try again!',
+      position: 'topRight',
     });
+  } finally {
+    refs.loader.classList.add('hidden');
+    e.target.reset();
+  }
 });
 
-function renderValue(items) {
+refs.loadMoreBtn.addEventListener('click', async () => {
+  page += 1;
+  refs.loader.classList.remove('hidden');
+
+  try {
+    const images = await fetchImages(query, page, perPage);
+    renderImages(images);
+    if (images.length < perPage) refs.loadMoreBtn.classList.add('hidden');
+  } catch (error) {
+    iziToast.error({
+      title: 'Error',
+      message: 'Error loading more images!',
+      position: 'topRight',
+    });
+  } finally {
+    refs.loader.classList.add('hidden');
+  }
+});
+
+function renderImages(items) {
   const markup = imagesTemplate(items);
   refs.container.insertAdjacentHTML('beforeend', markup);
-
   gallery.refresh();
 }
 
