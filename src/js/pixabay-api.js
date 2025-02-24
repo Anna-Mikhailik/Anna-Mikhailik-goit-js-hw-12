c15a143dbcd5fff77030ce3395da5523f28be5e2
annplay <messageforannolay@gmail.com>
2025-02-24 21:48:00 +0200

    032

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -4,7 +4,7 @@ const BASE_URL = 'https://pixabay.com/api/';
 const API_KEY = '48906528-865b601cba228060b80f58b51';
 const PER_PAGE = 40;
 
-export async function fetchImages(query, page) {
+export function fetchImages(query) {
   const params = {
     key: API_KEY,
     q: query,
@@ -12,14 +12,28 @@ export async function fetchImages(query, page) {
     orientation: 'horizontal',
     safesearch: true,
     per_page: PER_PAGE,
-    page: page,
   };
 
-  try {
-    const response = await axios.get(BASE_URL, { params });
-    return response.data;
-  } catch (error) {
-    console.error('Error fetching images:', error);
-    return { hits: [], totalHits: 0 };
-  }
+  return axios
+    .get(BASE_URL, { params })
+    .then(response => {
+      if (response.data.hits.length === 0) {
+        iziToast.error({
+          title: 'Error',
+          message:
+            'Sorry, there are no images matching your search query. Please try again!',
+          position: 'topRight',
+        });
+        return [];
+      }
+      return response.data.hits;
+    })
+    .catch(error => {
+      iziToast.error({
+        title: 'Error',
+        message: 'Something went wrong. Please try again later!',
+        position: 'topRight',
+      });
+      return [];
+    });
 }
