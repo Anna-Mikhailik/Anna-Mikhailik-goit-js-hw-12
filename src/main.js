b72bf38abc977f462efbb29f17507c9aff2b9960
annplay <messageforannolay@gmail.com>
2025-02-24 20:11:30 +0200

    022

--- src/main.js
+++ src/main.js
@@ -16,7 +16,6 @@ let query = '';
 let page = 1;
 const perPage = 40;
 let imagesBuffer = [];
-let totalHits = 0;
 
 refs.form.addEventListener('submit', async (e) => {
   e.preventDefault();
@@ -38,8 +37,7 @@ refs.form.addEventListener('submit', async (e) => {
   imagesBuffer = [];
   
   try {
-    const { images, totalHits: hits } = await fetchImages(query, page, perPage);
-    totalHits = hits;
+    const images = await fetchImages(query, page, perPage);
     imagesBuffer = images;
     renderImages(imagesBuffer.slice(0, 20));
     if (imagesBuffer.length > 20) refs.loadMoreBtn.classList.remove('hidden');
@@ -56,27 +54,18 @@ refs.form.addEventListener('submit', async (e) => {
 });
 
 refs.loadMoreBtn.addEventListener('click', async () => {
-  const currentImagesCount = refs.container.children.length;
-  const remainingImages = imagesBuffer.slice(currentImagesCount, currentImagesCount + 20);
+  const remainingImages = imagesBuffer.slice(refs.container.children.length, refs.container.children.length + 20);
   renderImages(remainingImages);
 
-  if (refs.container.children.length >= imagesBuffer.length && refs.container.children.length < totalHits) {
+  if (refs.container.children.length >= imagesBuffer.length) {
     page += 1;
     refs.loader.classList.remove('hidden');
     try {
-      const { images } = await fetchImages(query, page, perPage);
+      const images = await fetchImages(query, page, perPage);
       imagesBuffer = images;
       renderImages(imagesBuffer.slice(0, 20));
-      if (refs.container.children.length >= totalHits) {
-        refs.loadMoreBtn.classList.add('hidden');
-        iziToast.info({
-          title: 'Info',
-          message: "We're sorry, but you've reached the end of search results.",
-          position: 'topRight',
-        });
-      } else if (imagesBuffer.length > 20) {
-        refs.loadMoreBtn.classList.remove('hidden');
-      }
+      if (imagesBuffer.length > 20) refs.loadMoreBtn.classList.remove('hidden');
+      else refs.loadMoreBtn.classList.add('hidden');
     } catch (error) {
       iziToast.error({
         title: 'Error',
